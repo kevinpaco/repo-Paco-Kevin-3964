7845e3aafa0bd4312655272d520ac16ba987cb40
kevinpaco <kevinpaco40@gmail.com>
2022-05-10 00:03:17 -0300

    commit socio

--- ProjectDemo/src/main/java/ar/edu/unju/fi/pvisual/Empresa/Socio.java
+++ ProjectDemo/src/main/java/ar/edu/unju/fi/pvisual/Empresa/Socio.java
@@ -1,5 +1,10 @@
 package ar.edu.unju.fi.pvisual.Empresa;
 
 public class Socio {
+	public void hola(String[] args)
+	{
+		System.out.println("hola mundo");
+		System.out.println("soy kevin");
+	}
 
 }
